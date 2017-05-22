9c14cdb5b42abbd4c1403f95b21b6d3f6a7936a6
Ashish Datta <ashish@setfive.com>
2017-05-21 22:51:13 -0400

    Added a next up slate demo

---
+++ examples/findNextSlate.ts
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+import {dirname} from "path";
+import Fanduel from "../index";
+import * as Q from "q";
+import * as _ from "lodash";
+import {FanduelConfig} from "../models";
+import * as moment from "moment";
+
+/**
+ * Here we'll grab the available slates from Fanduel and output the one that's starting the soonest.
+ *
+ */
+
+const auth = JSON.parse(fs.readFileSync(dirname(__filename) + "/../auth.json", "utf8"));
+const fd : Fanduel = new Fanduel(<FanduelConfig> auth);
+
+fd.getAvailableSlates().then(slates => {
+    const sorted = _.sortBy(slates, f => {
+       const sd = moment(f.start_date);
+       return sd.unix();
+    });
+
+    if(sorted.length == 0){
+        console.log("Currently no available slates.");
+        process.exit(0);
+    }
+
+    console.log("Soonest slate is:")
+    console.log(sorted[0]);
+    process.exit(0);
+});
--- examples/websocket.ts
+++ examples/websocket.ts
@@ -5,6 +5,11 @@ import * as Q from "q";
 import * as _ from "lodash";
 import {FanduelConfig} from "../models";
 
+/**
+ * This just listens to the Fanduel "lobby" websocket for updates.
+ * It then filters for "Head to Head" contests with an entry fee less than $5.
+ */
+
 const auth = JSON.parse(fs.readFileSync(dirname(__filename) + "/../auth.json", "utf8"));
 const fd : Fanduel = new Fanduel(<FanduelConfig> auth);
 
